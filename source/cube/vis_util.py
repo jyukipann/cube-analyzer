0e9035f9226643af507fb668a246f45d126df7bb
jyukipann <jyukipann@yahoo.co.jp>
2025-05-21 18:21:41 +0900

    いったん

--- source/cube/vis_util.py
+++ source/cube/vis_util.py
@@ -40,6 +40,8 @@ C00, C01, C02, C03, C04, C05, C06, C07 = tuple(range(8))
 E00, E01, E02, E03, E04, E05, E06, E07, E08, E09, E10, E11 = tuple(range(12))
 # 面の位置
 F00, F01, F02, F03, F04, F05 = tuple(range(6))
+F, R, L, B, U, D = F00, F01, F02, F03, F04, F05
+
 # 面内でのサブキューブの位置
 FP00, FP01, FP02 =((0,0), (0,1), (0,2))
 FP10, FP11, FP12 =((1,0), (1,1), (1,2))
@@ -49,7 +51,7 @@ FP20, FP21, FP22 =((2,0), (2,1), (2,2))
 # 角と面の向きの関係
 # 面の番号が少ない方から時計回りが面の番号が入る
 # その場所にあるサブキューブの向きが入る
-CONER_FACES = torch.tensor([
+CORNER_FACES = torch.tensor([
     [F02, F03, F04], # 0 LUB
     [F01, F04, F03], # 1 RUB
     [F00, F04, F01], # 2 FUR
@@ -62,7 +64,7 @@ CONER_FACES = torch.tensor([
 
 # F00, F02, F20, F22のどれか
 # その角を正面から見て面の番号が最小のものから時計まわりに入れていく
-CONER_SUBCUBES = torch.tensor([
+CORNER_SUBCUBES = torch.tensor([
     [FP00, FP02, FP00], # 0
     [FP02, FP02, FP00], # 1
     [FP02, FP22, FP00], # 2
@@ -81,10 +83,24 @@ def state_to_net(state: State)->torch.Tensor:
         net[F03, *FP11], net[F04, *FP11], net[F05, *FP11],
     ) = tuple(range(6))
     
-    print(state.corner_positions)
-    coner_faces = CONER_FACES[state.corner_positions.to(torch.int64)]
-    print(coner_faces)
+    slicer_cp = state.corner_positions.to(torch.int64)
+    corner_faces = CORNER_FACES[slicer_cp]
+    corner_twists = state.twist_co
+    corner_subcube_positions = CORNER_SUBCUBES[slicer_cp]
+    print(corner_twists)
+    print(corner_faces)
+    # print(corner_subcube_positions)
     
+    # corner_facesをtwist分だけ回転させる
+    corner_twists = corner_twists.unsqueeze(1).view(-1, 1).to(torch.int64)
+    corner_faces_twisted = torch.empty_like(corner_faces)
+    print(corner_faces_twisted[:, 0])
+    print(corner_faces[:, (0+corner_twists[:, 0]) % 3])
+    print(corner_twists)
+    corner_faces_twisted[:, 0] = corner_faces[:, (0+corner_twists[:, 0]) % 3]
+    corner_faces_twisted[:, 1] = corner_faces[:, (1+corner_twists[:, 0]) % 3]
+    corner_faces_twisted[:, 2] = corner_faces[:, (2+corner_twists[:, 0]) % 3]
+    print(corner_faces_twisted)
     return net
     
     
@@ -102,4 +118,6 @@ if __name__ == "__main__":
             [1] * 12, dtype=torch.int8),
     )
     
+    state_to_net(State())
+    state_to_net(r)
     
